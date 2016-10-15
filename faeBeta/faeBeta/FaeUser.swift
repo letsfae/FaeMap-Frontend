7245ee10899d3c23238202b4ef5c52d15dce6b58
yancywang <yancywang@outlook.com>
2016-10-15 12:10:39 -0700

    te

--- faeBeta/faeBeta/FaeUser.swift
+++ faeBeta/faeBeta/FaeUser.swift
@@ -18,7 +18,7 @@ import Foundation
  // NSString! returned (optional)
  */
 class FaeUser : NSObject {
-    //test
+    //te
     var keyValue = [String:AnyObject]()
     override init (){
         //local storage
