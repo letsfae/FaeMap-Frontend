23e4210cdd9c63443abe6cfd72a484f7d6d3c3bd
YAYUAN SHI <yayuanshi@yayuans-macbook-pro.local>
2016-11-17 13:43:36 -0800

    Change some Anyobject to its real type in ChatViewController+CollectionView.swift

--- faeBeta/faeBeta/OutgoingMessage.swift
+++ faeBeta/faeBeta/OutgoingMessage.swift
@@ -75,7 +75,7 @@ class OutgoingMessage {
                 print("Error, couldn't send message: \(error)")
             }else{
                 //WARNING : I changed item["type"] to "text" here to make backend work
-                postToURL("chats", parameter: ["receiver_id": user.userId as AnyObject, "message": item["message"] as AnyObject, "type": "text" as AnyObject], authentication: headerAuthentication(), completion: { (statusCode, result) in
+                postToURL("chats", parameter: ["receiver_id": user.userId as AnyObject, "message": item["message"] as! String, "type": "text"], authentication: headerAuthentication(), completion: { (statusCode, result) in
                     if(statusCode / 100 == 2){
                         if let resultDic = result as? NSDictionary{
                             self.delegate.updateChat_Id((resultDic["chat_id"] as! NSNumber).stringValue)
