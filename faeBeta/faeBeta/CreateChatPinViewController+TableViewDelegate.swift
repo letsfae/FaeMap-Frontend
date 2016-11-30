2c2aec19d29e999ad47603708169b206a7aaf854
YAYUAN SHI <yayuanshi@yayuans-macbook-pro.local>
2016-11-29 21:57:09 -0800

    Set the options for create chat pin VC

--- faeBeta/faeBeta/CreateChatPinViewController+TableViewDelegate.swift
+++ faeBeta/faeBeta/CreateChatPinViewController+TableViewDelegate.swift
@@ -10,14 +10,41 @@ import UIKit
 
 extension CreateChatPinViewController : UITableViewDelegate, UITableViewDataSource{
     
+    enum CreateChatPinNormalOptions: Int {
+        case AddDescription = 0
+        case ChooseLocation = 1
+        case MoreOptions = 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CreatePinOptionsTableView.cellReuseIdentifier) as! CreatePinOptionsTableViewCell
+        switch indexPath.row{
+        case CreateChatPinNormalOptions.AddDescription.rawValue:
+            cell.setupCell(withTitle: "Add Description", leadingIcon: #imageLiteral(resourceName: "addDescription"), trailingText: nil, trailingIcon: #imageLiteral(resourceName: "plusIcon"))
+            break
+        case CreateChatPinNormalOptions.ChooseLocation.rawValue:
+            cell.setupCell(withTitle: "Choose Location", leadingIcon: #imageLiteral(resourceName: "pinSelectLocation01"), trailingText: nil, trailingIcon: #imageLiteral(resourceName: "whiteRightPointer"))
+
+            break
+        case CreateChatPinNormalOptions.MoreOptions.rawValue:
+            cell.setupCell(withTitle: "More Options", leadingIcon: #imageLiteral(resourceName: "optionsIcon"), trailingText: nil, trailingIcon: #imageLiteral(resourceName: "whiteRightPointer"))
+            break
+        default:
+            break
+        }
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
     
 }
