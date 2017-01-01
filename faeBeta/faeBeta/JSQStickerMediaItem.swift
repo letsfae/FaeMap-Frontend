e1b797ef6462e1102335bca76e0e3e88506833e2
YAYUAN SHI <yayuanshi@yayuans-macbook-pro.local>
2017-01-01 12:39:15 -0800

    Increase heart icon top&bottom clearance

--- faeBeta/faeBeta/JSQStickerMediaItem.swift
+++ faeBeta/faeBeta/JSQStickerMediaItem.swift
@@ -15,24 +15,34 @@ class JSQStickerMediaItem: JSQPhotoMediaItem {
     //inheritance from JSQPhoto
     var cachedImageView : UIImageView!
     var customizeSize : CGSize!
+    var heartButtonTopOffset: CGFloat = 10
     override func mediaView() -> UIView! {
         if self.image == nil {
             return nil
         }
         if self.cachedImageView == nil {
-            let imageSize = self.mediaViewDisplaySize()
-            let imageView = UIImageView(image: self.image)
-            imageView.frame = CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height)
-            imageView.contentMode = .scaleAspectFit
-//            JSQMessagesMediaViewBubbleImageMasker.applyBubbleImageMask(toMediaView: imageView, isOutgoing: self.appliesMediaViewMaskAsOutgoing)
-            self.cachedImageView = imageView
+            if customizeSize != nil{
+                let imageSize = self.mediaViewDisplaySize()
+                let view = UIImageView(frame: CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height))
+                let imageView = UIImageView(image: self.image)
+                imageView.frame = CGRect(x: 0, y: heartButtonTopOffset, width: imageSize.width, height: imageSize.height - heartButtonTopOffset * 2)
+                imageView.contentMode = .scaleAspectFit
+                view.addSubview(imageView)
+                self.cachedImageView = view 
+            }else{
+                let imageSize = self.mediaViewDisplaySize()
+                let imageView = UIImageView(image: self.image)
+                imageView.frame = CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height)
+                imageView.contentMode = .scaleAspectFit
+                self.cachedImageView = imageView
+            }
         }
         return cachedImageView
     }
     
     //overridde the photo size to the stick size
     override func mediaViewDisplaySize() -> CGSize {
-        return customizeSize ?? CGSize(width: 120, height: 90)
+        return customizeSize != nil ? CGSize(width: customizeSize.width, height: customizeSize.height + heartButtonTopOffset * 2) : CGSize(width: 120, height: 90)
     }
     
 }
