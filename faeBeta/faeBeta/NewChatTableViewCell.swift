c7a165dbc119e646d4645f3937c7a4dc313bfd5e
felixjichao <felixjichao@gmail.com>
2017-08-19 23:12:50 -0700

    [UPDATE]start new chat

--- faeBeta/faeBeta/NewChatTableViewCell.swift
+++ faeBeta/faeBeta/NewChatTableViewCell.swift
@@ -15,6 +15,7 @@ class NewChatTableViewCell: UITableViewCell {
     var lblUserName: UILabel!
     var imgStatus: UIImageView!
     //var imgSeparator: UIImageView!
+    var userID: Int = -1
     var statusSelected = false
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
@@ -23,7 +24,7 @@ class NewChatTableViewCell: UITableViewCell {
         
         imgAvatar = UIImageView()
         imgAvatar.image = #imageLiteral(resourceName: "avatarPlaceholder")
-        imgAvatar.layer.cornerRadius = 5
+        imgAvatar.layer.cornerRadius = 25
         imgAvatar.layer.masksToBounds = true
         addSubview(imgAvatar)
         addConstraintsWithFormat("H:|-15-[v0(50)]", options: [], views: imgAvatar)
@@ -31,26 +32,28 @@ class NewChatTableViewCell: UITableViewCell {
         
         lblNickName = UILabel()
         lblNickName.textAlignment = .left
-        lblNickName.textColor = UIColor._107105105()
+        lblNickName.textColor = UIColor._898989()
         lblNickName.font = UIFont(name: "AvenirNext-Medium", size: 18)
         addSubview(lblNickName)
         addConstraintsWithFormat("H:|-85-[v0(274)]", options: [], views: lblNickName)
-        addConstraintsWithFormat("V:|-21-[v0(12)]", options: [], views: lblNickName)
+        addConstraintsWithFormat("V:|-17-[v0(22)]", options: [], views: lblNickName)
         
         lblUserName = UILabel()
         lblUserName.textAlignment = .left
-        lblUserName.textColor = UIColor._898989()
+        lblUserName.textColor = UIColor._107105105()
         lblUserName.font = UIFont(name: "AvenirNext-Medium", size: 13)
         addSubview(lblUserName)
         addConstraintsWithFormat("H:|-85-[v0(274)]", options: [], views: lblUserName)
-        addConstraintsWithFormat("V:|-41-[v0(15)]", options: [], views: lblUserName)
+        addConstraintsWithFormat("V:|-38-[v0(20)]", options: [], views: lblUserName)
         
         imgStatus = UIImageView()
         imgStatus.image = #imageLiteral(resourceName: "status_unselected")
         imgStatus.layer.masksToBounds = true
         addSubview(imgStatus)
-        addConstraintsWithFormat("H:[v0(20)]-27-|", options: [], views: imgStatus)
-        addConstraintsWithFormat("V:|-34-[v0(20)]", options: [], views: imgStatus)
+        addConstraintsWithFormat("H:[v0(20)]-17-|", options: [], views: imgStatus)
+        addConstraintsWithFormat("V:|-27-[v0]-27-|", options: [], views: imgStatus)
+        
+        selectionStyle = .none
     }
     
     required init?(coder aDecoder: NSCoder) {
