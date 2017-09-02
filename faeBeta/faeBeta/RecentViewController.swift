aa45e1a7b8582e707799fe8a67d6b8014dabab43
felixjichao <felixjichao@gmail.com>
2017-09-01 19:55:10 -0700

    [FIX]indexing in the chat log

--- faeBeta/faeBeta/RecentViewController.swift
+++ faeBeta/faeBeta/RecentViewController.swift
@@ -253,13 +253,12 @@ class RecentViewController: UIViewController, UITableViewDataSource, UITableView
             self.navigationController?.pushViewController(chatVC, animated: true)
         }*/
         firebase.child(chatVC.chatRoomId).keepSynced(true)
-        firebase.child(chatVC.chatRoomId).queryLimited(toLast: UInt(15)).observeSingleEvent(of: .value) { (snapshot: DataSnapshot) in
-            let items = (snapshot.value as? NSDictionary)!
-            for item in items {
+        firebase.child(chatVC.chatRoomId).queryLimited(toLast: UInt(15)).queryOrdered(byChild: "index").observeSingleEvent(of: .value) { (snapshot: DataSnapshot) in
+            let items: NSEnumerator = snapshot.children
+            while let item = items.nextObject() as? DataSnapshot {
                 let message = (item.value as? NSMutableDictionary)!
-                message["keyValue"] = item.key
                 chatVC.messagesInit.append(message)
-            }            
+            }
             self.navigationController?.pushViewController(chatVC, animated: true)
         }
     }
