b2f6605b7bde114dca1bce66c12b27b4fc5ffd14
felixjichao <felixjichao@gmail.com>
2017-11-21 17:18:36 -0800

    [ADD] contacts - new scroll bar

--- faeBeta/faeBeta/Contacts/FaeContactsExtensions/ScrollingIndicatorChange.swift
+++ faeBeta/faeBeta/Contacts/FaeContactsExtensions/ScrollingIndicatorChange.swift
@@ -11,9 +11,23 @@ import UIKit
 extension ContactsViewController: UIScrollViewDelegate {
     
     func setupScrollBar() {
+        if tblContacts.contentSize.height - 74 > screenHeight - 114 - device_offset_top {
+            return
+        }
+        let uiviewScrollZone = UIView(frame: CGRect(x: screenWidth - 23, y: 120 + device_offset_top, width: 23, height: screenHeight - 120 - device_offset_top - 6))
+        uiviewScrollZone.backgroundColor = .clear
+        view.addSubview(uiviewScrollZone)
+        let longpressGestureZone = UILongPressGestureRecognizer(target: self, action: #selector(handleScrollZoneLongPress(_:)))
+        uiviewScrollZone.addGestureRecognizer(longpressGestureZone)
+        
         view.addSubview(btnIndicator)
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureIndicator(_:)))
+        btnIndicator.addSubview(lblPrefix)
+        lblPrefix.isHidden = true
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(longPressGestureRecognizer(_:)))
+        let longpressGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPressGestureRecognizer(_:)))
+        longpressGesture.minimumPressDuration = 0.3
         btnIndicator.addGestureRecognizer(panGesture)
+        btnIndicator.addGestureRecognizer(longpressGesture)
     }
     
     /* Joshua 06/18/17
@@ -23,7 +37,7 @@ extension ContactsViewController: UIScrollViewDelegate {
     
     // UIScrollViewDelegate
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        felixprint("[scrollViewWillBeginDragging]")
+        //felixprint("[scrollViewWillBeginDragging]")
         
         //animateIndicatorSize(type: 1)
         
@@ -38,14 +52,23 @@ extension ContactsViewController: UIScrollViewDelegate {
         let scrollHeight = scrollView.contentSize.height
         let percentage = currentOffset / (scrollHeight - tableHeight)
         //felixprint(percentage)
-        let btnRange = screenHeight - 120 - 30 - 6 - device_offset_top - device_offset_bot
+        
         if percentage <= 0 {
             btnIndicator.frame.origin.y = 120 + device_offset_top
         } else if percentage < 1 {
-            btnIndicator.frame.origin.y = 120 + btnRange * percentage + device_offset_top
+            btnIndicator.frame.origin.y = 120 + floatBtnRange * percentage + device_offset_top
         } else {
-            btnIndicator.frame.origin.y = 120 + btnRange + device_offset_top
+            btnIndicator.frame.origin.y = 120 + floatBtnRange + device_offset_top
         }
+        let scrollVelocity = scrollView.panGestureRecognizer.velocity(in: scrollView)
+        if fabs(scrollVelocity.y) > 300 && !scrollView.isAtTop && !scrollView.isAtBottom {
+            animateIndicatorSize(type: 1)
+        }
+        let showIndex = scrollView.isAtTop ? 0 : 1
+        let cell = (tblContacts.visibleCells[showIndex]) as? FaeContactsCell
+        guard let prefix = cell?.lblUserName?.text else { return }
+        lblPrefix.text = (prefix as NSString).substring(to: 1)
+        //print(scrollVelocity)
         //animateIndicatorSize(type: 2)
         // scrollView's last subview is the scrolling indicator
         // this indicator exists when table is initialized
@@ -92,9 +115,9 @@ extension ContactsViewController: UIScrollViewDelegate {
     }
     
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        felixprint("[scrollViewDidEndDragging]", decelerate)
+        //felixprint("[scrollViewDidEndDragging]", decelerate)
         indicatorState = .end
-        
+        animateIndicatorSize(type: 2)
         // check if you can find when will "decelerate" be set to true or false
         /*if !decelerate {
             animateIndicatorSize(type: 2)
@@ -103,7 +126,8 @@ extension ContactsViewController: UIScrollViewDelegate {
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         // highly suggest to check the console to see this line of print when trigger srolling
-        felixprint("[scrollViewDidEndDecelerating]")
+        //felixprint("[scrollViewDidEndDecelerating]")
+        animateIndicatorSize(type: 2)
         /*if indicatorState == .end {
             UIView.animate(withDuration: 0.2, animations: {
                 self.btnIndicator.frame.size.width = 3
@@ -115,8 +139,72 @@ extension ContactsViewController: UIScrollViewDelegate {
     }
     // End of UIScrollViewDelegate
     
+    @objc func longPressGestureRecognizer(_ recognizer: UIGestureRecognizer) {
+        let currentPos = recognizer.location(in: nil).y
+        switch recognizer.state {
+        case .began:
+            animateIndicatorSize(type: 0)
+            floatLongpressStart = currentPos
+            floatFingerToBtnTop = currentPos - btnIndicator.frame.origin.y
+            //felixprint(floatFingerToBtnTop)
+            break
+        case .changed:
+            //animateIndicatorSize(type: 0)
+            //felixprint(currentPos)
+            if currentPos < 120 + floatFingerToBtnTop || currentPos > screenHeight - 6 - 30 + floatFingerToBtnTop {
+                break
+            }
+            btnIndicator.frame.origin.y = currentPos - floatFingerToBtnTop
+            var percentage = (btnIndicator.frame.origin.y - 120) / floatBtnRange
+            let scrollRange = tblContacts.contentSize.height - tblContacts.frame.height
+            if percentage > 0.995 {
+                percentage = 1.0
+            }
+            if percentage < 0.005 {
+                percentage = 0.0
+            }
+            tblContacts.setContentOffset(CGPoint(x: 0, y: scrollRange * percentage), animated: false)
+            //felixprint(percentage)
+            break
+        case .ended:
+            animateIndicatorSize(type: 2)
+            break
+        default:
+            animateIndicatorSize(type: 2)
+            break
+        }
+    }
+    
     @objc func panGestureIndicator(_ pan: UIPanGestureRecognizer) {
-        if pan.state == .began {
+        let currentPos = pan.location(in: nil).y
+        switch pan.state {
+        case .began:
+            animateIndicatorSize(type: 0)
+            floatFingerToBtnTop = currentPos - btnIndicator.frame.origin.y
+            break
+        case .changed:
+            if currentPos < 120 + floatFingerToBtnTop || currentPos > screenHeight - 6 - 30 + floatFingerToBtnTop {
+                break
+            }
+            btnIndicator.frame.origin.y = currentPos - floatFingerToBtnTop
+            var percentage = (btnIndicator.frame.origin.y - 120) / floatBtnRange
+            let scrollRange = tblContacts.contentSize.height - tblContacts.frame.height
+            if percentage > 0.995 {
+                percentage = 1.0
+            }
+            if percentage < 0.005 {
+                percentage = 0.0
+            }
+            tblContacts.setContentOffset(CGPoint(x: 0, y: scrollRange * percentage), animated: false)
+            break
+        case .ended:
+            animateIndicatorSize(type: 2)
+            break
+        default:
+            animateIndicatorSize(type: 2)
+            break
+        }
+        /*if pan.state == .began {
             animateIndicatorSize(type: 0)
             indicatorState = .scrolling
         } else if pan.state == .ended || pan.state == .failed || pan.state == .cancelled {
@@ -158,47 +246,135 @@ extension ContactsViewController: UIScrollViewDelegate {
             let point = CGPoint(x: 0, y: point_y)
             
             tblContacts.setContentOffset(point, animated: false)
+        }*/
+    }
+    
+    @objc func handleScrollZoneLongPress(_ recognizer: UILongPressGestureRecognizer) {
+        let currentPos = recognizer.location(in: nil).y
+        switch recognizer.state {
+        case .began:
+            floatFingerToBtnTop = 15.0
+            if currentPos >= 120 && currentPos <= 120 + 30 {
+                tblContacts.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
+                UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseOut], animations: {
+                    self.btnIndicator.frame.origin.y = 120 + device_offset_top
+                })
+            } else if currentPos >= screenHeight - 6 - 30 && currentPos <= screenHeight - 6 {
+                tblContacts.setContentOffset(CGPoint(x: 0, y: tblContacts.contentSize.height - tblContacts.frame.height), animated: false)
+                UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseOut], animations: {
+                    self.btnIndicator.frame.origin.y = screenHeight - 6 - 30
+                })
+            } else {
+                let percentage = (currentPos - 15 - 120) / floatBtnRange
+                let scrollRange = tblContacts.contentSize.height - tblContacts.frame.height
+                tblContacts.setContentOffset(CGPoint(x: 0, y: scrollRange * percentage), animated: false)
+                UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseOut], animations: {
+                    self.btnIndicator.frame.origin.y = currentPos - 15
+                })
+            }
+            animateIndicatorSize(type: 0)
+            //felixprint(floatFingerToBtnTop)
+            break
+        case .changed:
+            //animateIndicatorSize(type: 0)
+            //felixprint(currentPos)
+            if currentPos < 120 + floatFingerToBtnTop || currentPos > screenHeight - 6 - 30 + floatFingerToBtnTop {
+                break
+            }
+            btnIndicator.frame.origin.y = currentPos - floatFingerToBtnTop
+            var percentage = (btnIndicator.frame.origin.y - 120) / floatBtnRange
+            let scrollRange = tblContacts.contentSize.height - tblContacts.frame.height
+            if percentage > 0.995 {
+                percentage = 1.0
+            }
+            if percentage < 0.005 {
+                percentage = 0.0
+            }
+            tblContacts.setContentOffset(CGPoint(x: 0, y: scrollRange * percentage), animated: false)
+            //felixprint(percentage)
+            break
+        case .ended:
+            animateIndicatorSize(type: 2)
+            break
+        default:
+            animateIndicatorSize(type: 2)
+            break
         }
     }
     
     func animateIndicatorSize(type: Int) {
         // cornerRadius is not animatable
-        
+        let uiviewBar = btnIndicator.subviews[0]
         switch type {
             
         // width = 90, height = 30
         case 0:
-            self.btnIndicator.layer.cornerRadius = 5
-            
-            UIView.animate(withDuration: 0.2, animations: {
-                self.btnIndicator.frame.size.width = 90
-                self.btnIndicator.frame.origin.x = 319
+            //self.btnIndicator.layer.cornerRadius = 5
+            uiviewBar.layer.cornerRadius = 5
+            UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseOut], animations: {
+                uiviewBar.frame.size.width = 90
+                uiviewBar.frame.origin.x = 3 + 15 - 90
+                self.lblPrefix.frame.origin.x = 3 + 15 - 90 + 6
+                self.lblPrefix.isHidden = false
+                //self.btnIndicator.frame.size.width = 90
+                //self.btnIndicator.frame.origin.x = 319
             }, completion: nil)
             break
             
         // width = 60, height = 30
         case 1:
-            self.btnIndicator.layer.cornerRadius = 5
-            
+            //self.btnIndicator.layer.cornerRadius = 5
+            uiviewBar.layer.cornerRadius = 5
             // this animation is only for dragging table cells
             // not for tapping or dragging the scrolling button
-            UIView.animate(withDuration: 0.2, animations: {
-                self.btnIndicator.frame.size.width = 60
-                self.btnIndicator.frame.origin.x = 349
+            UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseOut], animations: {
+                uiviewBar.frame.size.width = 50
+                uiviewBar.frame.origin.x = 3 + 15 - 50
+                self.lblPrefix.frame.origin.x = 3 + 15 - 50 + 6
+                self.lblPrefix.isHidden = false
+                //self.btnIndicator.frame.size.width = 60
+                //self.btnIndicator.frame.origin.x = 349
             })
             break
             
         // width = 3, height = 30
         case 2:
-            UIView.animate(withDuration: 0.2, animations: {
-                self.btnIndicator.frame.size.width = 3
-                self.btnIndicator.frame.origin.x = 406
+            lblPrefix.isHidden = true
+            UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseIn], animations: {
+                //self.btnIndicator.frame.size.width = 3
+                //self.btnIndicator.frame.origin.x = screenWidth - 8
+                uiviewBar.frame.size.width = 3
+                uiviewBar.frame.origin.x = 15
             }) { _ in
-                self.btnIndicator.layer.cornerRadius = 3
+                //self.btnIndicator.layer.cornerRadius = 3
+                uiviewBar.layer.cornerRadius = 3
             }
             break
         default:
             break
         }
+    }
+}
+
+extension UIScrollView {
+    var isAtTop: Bool {
+        return contentOffset.y <= verticalOffsetForTop
+    }
+    
+    var isAtBottom: Bool {
+        return contentOffset.y >= verticalOffsetForBottom
+    }
+    
+    var verticalOffsetForTop: CGFloat {
+        let topInset = contentInset.top
+        return -topInset
+    }
+    
+    var verticalOffsetForBottom: CGFloat {
+        let scrollViewHeight = bounds.height
+        let scrollContentSizeHeight = contentSize.height
+        let bottomInset = contentInset.bottom
+        let scrollViewBottomOffset = scrollContentSizeHeight + bottomInset - scrollViewHeight
+        return scrollViewBottomOffset
     }
 }
