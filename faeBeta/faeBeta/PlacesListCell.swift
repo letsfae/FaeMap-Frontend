c9fdb6c26c61d2407a37222c708eae05a7c62753
Vicky <xiewei.iris@gmail.com>
2018-01-24 21:32:11 -0800

    [UPDATE] Update some features of map search. (e.g.: Display categories when typing in search places field, showing results when selecting category cell.) Update UI of Search part.

--- faeBeta/faeBeta/PlacesListCell.swift
+++ faeBeta/faeBeta/PlacesListCell.swift
@@ -37,7 +37,7 @@ class PlacesListCell: UITableViewCell {
     
     fileprivate func loadRecommendedCellContent() {
         imgPic = UIImageView()
-        imgPic.frame = CGRect(x: 15 * screenWidthFactor, y: 11, width: 46, height: 46)
+        imgPic.frame = CGRect(x: 12 * screenWidthFactor, y: 12, width: 48, height: 48)
         imgPic.contentMode = .scaleAspectFill
         imgPic.clipsToBounds = true
         addSubview(imgPic)
@@ -58,6 +58,53 @@ class PlacesListCell: UITableViewCell {
         addSubview(lblAddress)
         addConstraintsWithFormat("H:|-\(73*screenWidthFactor)-[v0]-\(20*screenWidthFactor)-|", options: [], views: lblAddress)
         addConstraintsWithFormat("V:|-16-[v0(20)]-0-[v1(16)]", options: [], views: lblPlaceName, lblAddress)
+        
+        bottomLine = UIView()
+        bottomLine.backgroundColor = UIColor._200199204()
+        addSubview(bottomLine)
+        addConstraintsWithFormat("H:|-\(69*screenWidthFactor)-[v0]-0-|", options: [], views: bottomLine)
+        addConstraintsWithFormat("V:[v0(1)]-0-|", options: [], views: bottomLine)
+    }
+}
+
+class CategoryListCell: UITableViewCell {
+    
+    var imgPic: UIImageView!
+    var lblCatName: UILabel!
+    var bottomLine: UIView!
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        separatorInset = UIEdgeInsets.zero
+        layoutMargins = UIEdgeInsets.zero
+        selectionStyle = .none
+        loadCellContent()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setValueForCategory(_ cat: (key: String, value: Int)) {
+        imgPic.image = UIImage(named: "place_result_\(cat.value)")
+        lblCatName.text = cat.key
+    }
+    
+    func loadCellContent() {
+        imgPic = UIImageView()
+        imgPic.frame = CGRect(x: 7 * screenWidthFactor, y: 7, width: 58, height: 58)
+        imgPic.contentMode = .scaleAspectFill
+        imgPic.clipsToBounds = true
+        addSubview(imgPic)
+        
+        lblCatName = UILabel()
+        lblCatName.textAlignment = .left
+        lblCatName.lineBreakMode = .byTruncatingTail
+        lblCatName.textColor = UIColor._898989()
+        lblCatName.font = UIFont(name: "AvenirNext-Medium", size: 15)
+        addSubview(lblCatName)
+        addConstraintsWithFormat("H:|-\(73*screenWidthFactor)-[v0]-\(20*screenWidthFactor)-|", options: [], views: lblCatName)
+        addConstraintsWithFormat("V:|-24-[v0]", options: [], views: lblCatName)
         
         bottomLine = UIView()
         bottomLine.backgroundColor = UIColor._200199204()
